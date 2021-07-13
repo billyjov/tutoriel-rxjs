a2e57a4bf2acd8cbd351102c00e5fe9f37172ea8
Billy Lando <billy_lando@yahoo.com>
2021-07-13 13:40:10 +0200

    feat: add part-4

--- rxjs-guru/src/app/app.component.ts
+++ rxjs-guru/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,22 @@ export class AppComponent implements OnInit {
       error: (err: unknown) => console.log(`Oups il ya une erreur ${err}`),
       complete: () => console.log('terminé...plus rien')
     };
+
+    const stream = new Observable(myObserver => {
+      myObserver.next('Boite 1');
+      myObserver.next('Boite 2');
+      myObserver.next('Boite 3');
+      // for (let index = 0; index < 1000000; index++) {
+      //   myObserver.next('Boite');
+      // }
+      myObserver.complete();
+    });
+
+    const subscription = stream.subscribe(
+      item => console.log(`Une boite arrive ${item}`),
+      err => console.log(`Oups il ya une erreur ${err}`),
+      () => console.log('terminé...plus rien')
+    );
 
   }
 }
