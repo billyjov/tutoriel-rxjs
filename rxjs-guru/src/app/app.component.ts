8f64863c1eb54be4ee3c6a785f4a503eb4c80342
Billy Lando <billy_lando@yahoo.com>
2021-07-13 13:05:16 +0200

    feat: add part-3

--- rxjs-guru/src/app/app.component.ts
+++ rxjs-guru/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rxjs-guru';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+
+    const observer = {
+      next: (item: unknown) => console.log(`Une boite arrive ${item}`),
+      error: (err: unknown) => console.log(`Oups il ya une erreur ${err}`),
+      complete: () => console.log('terminé...plus rien')
+    };
+
+  }
 }
